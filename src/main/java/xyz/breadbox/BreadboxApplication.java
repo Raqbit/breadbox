b01b9dc05613ff2517f228209735a26e22fd4e17
Bram Ceulemans <bramceulemans@me.com>
2018-06-14 14:46:48 +0200

    Initial commit

---
+++ src/main/java/xyz/breadbox/BreadboxApplication.java
@@ -0,0 +1,13 @@
+package xyz.breadbox;
+
+public class BreadboxApplication {
+
+    /**
+     * Run the Discord Bot.
+     *
+     * @param args the command line parameters
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
